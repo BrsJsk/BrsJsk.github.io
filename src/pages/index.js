ecafd6334d3f3c0f8879ed5053fa7415256e84a7
BrsJsk <joskicboris95@gmail.com>
2021-08-27 11:08:55 +0200

    Init page

---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+
+// styles
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+};
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+};
+
+// markup
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <title>Home Page</title>
+      <h1 style={headingStyles}>
+        <span role="img" aria-label="Party popper emojis">
+          🎉🎉🎉
+        </span>
+      </h1>
+    </main>
+  );
+};
+
+export default IndexPage;
