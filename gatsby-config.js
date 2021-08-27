cee71ac76f780289bf3e8cb26a2d7974177087df
BrsJsk <joskicboris95@gmail.com>
2021-08-27 13:35:43 +0200

    Add typescript and new header

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,13 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
   ],
 };
