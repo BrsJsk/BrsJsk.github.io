7db9653fe6d95564aedf04a14a211db38666ec60
BrsJsk <BrsJsk@users.noreply.github.com>
2021-09-06 13:23:48 +0000

    deployed via Gatsby Publish Action 🎩 for 9f5621e5728bf8d3b6bed40ae896305470e812aa

---
+++ component---src-pages-404-js-74872245ad2926bacbf4.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbrsjsk_starter_blog=self.webpackChunkbrsjsk_starter_blog||[]).push([[883],{9616:function(t,e,a){a.r(e);var n=a(7294),s=a(7198),l=a(3751);e.default=function(t){var e=t.data,a=t.location,r=e.site.siteMetadata.title;return n.createElement(s.Z,{location:a,title:r},n.createElement(l.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-74872245ad2926bacbf4.js.map
